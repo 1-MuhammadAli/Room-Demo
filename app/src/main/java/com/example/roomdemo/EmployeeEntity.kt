f5e4d26371d4bef9b667f283a82856bbbbff783e
1-MuhammadAli <ali.oraxtech@gmail.com>
2023-04-19 16:47:41 +0500

    initial commit

---
+++ app/src/main/java/com/example/roomdemo/EmployeeEntity.kt
@@ -0,0 +1,12 @@
+package com.example.roomdemo
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "employee-table")
+data class EmployeeEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+
+
+)
