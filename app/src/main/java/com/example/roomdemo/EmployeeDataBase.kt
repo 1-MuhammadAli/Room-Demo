df50c109d877ca9d9f04b61d1d012faa0518e28c
1-MuhammadAli <ali.oraxtech@gmail.com>
2023-04-26 15:11:02 +0500

    Employee Entity : Added ->  Setting up the Data Class - Room Entity

---
+++ app/src/main/java/com/example/roomdemo/EmployeeDataBase.kt
@@ -0,0 +1,41 @@
+package com.example.roomdemo
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [EmployeeEntity::class], version = 1)
+abstract class EmployeeDataBase:RoomDatabase() {
+
+    abstract fun employeeDao():EmployeeDao
+
+    companion object{
+
+        private  var INSTANCE: EmployeeDataBase? = null
+
+        fun getInstance(context: Context):EmployeeDataBase{
+
+            synchronized(this){
+                var instance = INSTANCE
+
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        EmployeeDataBase::class.java,
+                        "employee_database"
+
+                    ).fallbackToDestructiveMigration()
+                        .build()
+
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/roomdemo/EmployeeEntity.kt
+++ app/src/main/java/com/example/roomdemo/EmployeeEntity.kt
@@ -1,12 +1,17 @@
 package com.example.roomdemo
 
+import android.provider.ContactsContract.CommonDataKinds.Email
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "employee-table")
 data class EmployeeEntity(
     @PrimaryKey(autoGenerate = true)
-    val id: Int = 0
+    val id: Int = 0,
+    val name: String = "",
+    @ColumnInfo(name = "email-id")
+    val email: String = ""
 
 
 )
